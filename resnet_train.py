74084cf35e15c8b747bd0c4d5d4a045bf16acf41
cao <cao_1124@163.com>
2022-05-29 14:14:04 +0800

    fix bs

--- resnet_train.py
+++ resnet_train.py
@@ -70,9 +70,9 @@ def prepare_train(data_dir):
     train_data_size = len(train_dataset.indices)
     valid_data_size = len(val_dataset.indices)
 
-    train_data = DataLoader(train_dataset, batch_size=58,
+    train_data = DataLoader(train_dataset, batch_size=52,
                             shuffle=True, num_workers=8)
-    valid_data = DataLoader(val_dataset, batch_size=58,
+    valid_data = DataLoader(val_dataset, batch_size=52,
                             shuffle=False, num_workers=8)
 
     print(train_data_size, valid_data_size)
