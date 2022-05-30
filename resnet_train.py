ce082d028607c69ac621293e071746715f07b4d8
cao <cao_1124@163.com>
2022-05-30 11:45:12 +0800

    fix densenet121

--- resnet_train.py
+++ resnet_train.py
@@ -71,21 +71,26 @@ def prepare_train(data_dir):
     train_data_size = len(train_dataset.indices)
     valid_data_size = len(val_dataset.indices)
 
-    train_data = DataLoader(train_dataset, batch_size=32,
+    train_data = DataLoader(train_dataset, batch_size=64,
                             shuffle=True, num_workers=8)
-    valid_data = DataLoader(val_dataset, batch_size=32,
+    valid_data = DataLoader(val_dataset, batch_size=64,
                             shuffle=False, num_workers=8)
 
     print(train_data_size, valid_data_size)
 
     # 迁移学习  这里使用ResNet-50的预训练模型。
-    resnet = models.resnet50(pretrained=True)
-
-    resnet.fc = nn.Linear(in_features=2048, out_features=22, bias=True)
+    resnet = models.densenet121(pretrained=True)
+    resnet.classifier = nn.Linear(in_features=1024, out_features=22, bias=True)
+    # resnet.fc = nn.Linear(in_features=2048, out_features=22, bias=True)
     # renet18 resnet34
     # (fc): nn.Linear(in_features=512, out_features=1000, bias=True)
     # resnet50 resnet101  resnet152 resnext50_32x4d wide_resnet50_2  resnext101_32x8d  wide_resnet101_2
     # (fc): nn.Linear(in_features=2048, out_features=22, bias=True)
+    # densenet 121  in_features=1024
+    # densenet 161  in_features=2208
+    # densenet 169  in_features=1664
+    # densenet 201  in_features=1920
+    # (classifier): Linear(in_features=1024, out_features=22, bias=True)
     resnet.to(device)
 
     # 定义损失函数和优化器。
