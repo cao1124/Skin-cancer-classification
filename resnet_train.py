e3798e944b8a7e8f473b8571b0bb71ae47417864
cao <cao_1124@163.com>
2022-06-02 14:30:35 +0800

    del dropout

--- resnet_train.py
+++ resnet_train.py
@@ -80,12 +80,12 @@ def prepare_train(data_dir):
 
     # 迁移学习  这里使用ResNet-50的预训练模型。
     model = models.resnet50(pretrained=True)
-    # model.fc = nn.Linear(in_features=2048, out_features=22, bias=True)
-    model.fc = nn.Sequential(OrderedDict([('fc1', nn.Linear(2048, 128)),
-                                          ('relu1', nn.ReLU()),
-                                          ('dropout1', nn.Dropout(0.8)),
-                                          ('fc2', nn.Linear(128, 22)),
-                                          ('output', nn.Softmax(dim=1))]))
+    model.fc = nn.Linear(in_features=2048, out_features=22, bias=True)
+    # model.fc = nn.Sequential(OrderedDict([('fc1', nn.Linear(2048, 128)),
+    #                                       ('relu1', nn.ReLU()),
+    #                                       ('dropout1', nn.Dropout(1)),
+    #                                       ('fc2', nn.Linear(128, 22)),
+    #                                       ('output', nn.Softmax(dim=1))]))
     # for  vit_b_16 vit_l_16
     # model.heads = nn.Sequential(OrderedDict([('head', nn.Linear(in_features=1024, out_features=22, bias=True))]))
     # model = densenet264(pretrained=True)
