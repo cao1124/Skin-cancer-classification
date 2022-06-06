7922cea81417ae0e6af3d6bee02243f059cbeadf
cao <cao_1124@163.com>
2022-06-06 17:10:28 +0800

    fix train

--- resnet_train.py
+++ resnet_train.py
@@ -109,13 +109,14 @@ def prepare_train(data_dir):
     return dataset, model, optimizer, scheduler, loss_func
 
 
-def train_and_valid(dataset, model, optimizer, scheduler, loss_function, epochs=25):
-    # random split dataset 五折交叉验证
-    n_val = int(len(dataset) * 0.2)
-    n_train = len(dataset) - n_val
+def train_and_valid(data_dir, epochs=25):
     seed_list = [5, 4, 3, 2, 1]
     for i in seed_list:
         logger.info('第{}次实验:'.format(i))
+        dataset, model, optimizer, scheduler, loss_function = prepare_train(data_dir)
+        # random split dataset 五折交叉验证
+        n_val = int(len(dataset) * 0.2)
+        n_train = len(dataset) - n_val
         train_dataset, val_dataset = random_split(dataset, lengths=[n_train, n_val], generator=torch.manual_seed(i))
         train_data_size = len(train_dataset.indices)
         valid_data_size = len(val_dataset.indices)
@@ -203,8 +204,7 @@ if __name__ == '__main__':
 
     num_epochs = 100
     data_dir = 'data/us_label_crop/'
-    dataset, model, optimizer, scheduler, loss_func = prepare_train(data_dir)
-    train_and_valid(dataset, model, optimizer, scheduler, loss_func, num_epochs)
+    train_and_valid(data_dir, num_epochs)
 
     # plt show
     # trained_model, history =train_and_valid(dataset, model, optimizer, scheduler, loss_func, num_epochs)
