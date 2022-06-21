2f9477619a943641686573dddf9929b0efe842f9
cao <cao_1124@163.com>
2022-06-21 17:11:26 +0800

    test malignant 9classes

--- data_loader/inaturalist_data_loaders.py
+++ data_loader/inaturalist_data_loaders.py
@@ -100,7 +100,7 @@ class iNaturalistDataLoader(DataLoader):
         if training:
             # dataset = LT_Dataset(data_dir, data_dir + '/iNaturalist18_train.txt', train_trsfm)
             # val_dataset = LT_Dataset(data_dir, data_dir + '/iNaturalist18_val.txt', test_trsfm)
-            dataset = LT_Dataset(data_dir, data_dir + '/benign.txt', train_trsfm)
+            dataset = LT_Dataset(data_dir, data_dir + '/malignant.txt', train_trsfm)
             n_val = int(len(dataset) * 0.2)
             n_train = len(dataset) - n_val
             train_dataset, val_dataset = random_split(dataset, lengths=[n_train, n_val], generator=torch.Generator().manual_seed(0))
@@ -112,7 +112,7 @@ class iNaturalistDataLoader(DataLoader):
                 break
 
         else:   # test
-            dataset = LT_Dataset(data_dir, data_dir + '/benign.txt', test_trsfm)
+            dataset = LT_Dataset(data_dir, data_dir + '/malignant.txt', test_trsfm)
             n_val = int(len(dataset) * 0.2)
             n_train = len(dataset) - n_val
             train_dataset, val_dataset = random_split(dataset, lengths=[n_train, n_val], generator=torch.Generator().manual_seed(0))
@@ -132,7 +132,7 @@ class iNaturalistDataLoader(DataLoader):
             target_list.append(dataset.targets[i])
         num_classes = len(np.unique(target_list))
         # assert num_classes == 8142
-        assert num_classes == 13
+        assert num_classes == 9
 
         cls_num_list = [0] * num_classes
         for label in target_list:
