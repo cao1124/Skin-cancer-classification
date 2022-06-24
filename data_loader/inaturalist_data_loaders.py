43e8e33faf2ccd7e4351f74cb73abcde370b9a5d
cao <cao_1124@163.com>
2022-06-24 10:07:24 +0800

    test

--- data_loader/inaturalist_data_loaders.py
+++ data_loader/inaturalist_data_loaders.py
@@ -19,7 +19,7 @@ class BalancedSampler(Sampler):
         self.buckets = buckets
         self.bucket_pointers = [0 for _ in range(self.bucket_num)]
         self.retain_epoch_size = retain_epoch_size
-    
+
     def __iter__(self):
         count = self.__len__()
         while count > 0:
@@ -38,9 +38,10 @@ class BalancedSampler(Sampler):
 
     def __len__(self):
         if self.retain_epoch_size:
-            return sum([len(bucket) for bucket in self.buckets]) # Acrually we need to upscale to next full batch
+            return sum([len(bucket) for bucket in self.buckets])  # Acrually we need to upscale to next full batch
         else:
-            return max([len(bucket) for bucket in self.buckets]) * self.bucket_num # Ensures every instance has the chance to be visited in an epoch
+            return max([len(bucket) for bucket in
+                        self.buckets]) * self.bucket_num  # Ensures every instance has the chance to be visited in an epoch
 
 
 class LT_Dataset(Dataset):
@@ -48,25 +49,25 @@ class LT_Dataset(Dataset):
         self.img_path = []
         self.labels = []
         self.transform = transform
-        with open(txt, 'r', encoding='utf-8') as f:
+        with open(txt, 'r', encoding='gb2312') as f:
             for line in f:
                 # self.img_path.append(os.path.join(root, line.split()[0]))
                 # self.labels.append(int(line.split()[1]))
                 self.img_path.append(os.path.join(root, line.split(',')[0]))
                 self.labels.append(int(line.split(',')[1]))
         self.targets = self.labels  # Sampler needs to use targets
-        
+
     def __len__(self):
         return len(self.labels)
-        
+
     def __getitem__(self, index):
 
         path = self.img_path[index]
         label = self.labels[index]
-        
+
         with open(path, 'rb') as f:
             sample = Image.open(f).convert('RGB')
-        
+
         if self.transform is not None:
             sample = self.transform(sample)
 
@@ -78,7 +79,9 @@ class iNaturalistDataLoader(DataLoader):
     """
     iNaturalist Data Loader
     """
-    def __init__(self, data_dir, batch_size, shuffle=True, num_workers=1, training=True, balanced=False, retain_epoch_size=True):
+
+    def __init__(self, data_dir, batch_size, shuffle=True, num_workers=1, training=True, balanced=False,
+                 retain_epoch_size=True):
         train_trsfm = transforms.Compose([
             transforms.Resize([224, 224]),
             transforms.RandomHorizontalFlip(p=0.5),
@@ -91,7 +94,7 @@ class iNaturalistDataLoader(DataLoader):
             transforms.Normalize(skin_mean, skin_std)
         ])
         test_trsfm = transforms.Compose([
-            transforms.Resize([224, 224]),    # 256
+            transforms.Resize([224, 224]),  # 256
             # transforms.CenterCrop(224),
             transforms.ToTensor(),
             transforms.Normalize(skin_mean, skin_std)
@@ -101,22 +104,24 @@ class iNaturalistDataLoader(DataLoader):
         if training:
             # dataset = LT_Dataset(data_dir, data_dir + '/iNaturalist18_train.txt', train_trsfm)
             # val_dataset = LT_Dataset(data_dir, data_dir + '/iNaturalist18_val.txt', test_trsfm)
-            dataset = LT_Dataset(data_dir, data_dir + '/two-class.txt', train_trsfm)
+            dataset = LT_Dataset(data_dir, data_dir + '/malignant.txt', train_trsfm)
             n_val = int(len(dataset) * 0.2)
             n_train = len(dataset) - n_val
-            train_dataset, val_dataset = random_split(dataset, lengths=[n_train, n_val], generator=torch.Generator().manual_seed(0))
+            train_dataset, val_dataset = random_split(dataset, lengths=[n_train, n_val],
+                                                      generator=torch.Generator().manual_seed(0))
             # sklearn flod 五折交叉验证
-            train_dataset.indices, val_dataset.indices = [], []
             skf = StratifiedKFold(n_splits=5, random_state=None, shuffle=False)
             for train_index, val_index in skf.split(dataset.img_path, dataset.labels):
-                train_dataset.indices.append(list(train_index))
-                val_dataset.indices.append(list(val_index))
+                train_dataset.indices = list(train_index)
+                val_dataset.indices = list(val_index)
+                break
 
-        else:   # test
-            dataset = LT_Dataset(data_dir, data_dir + '/two-class.txt', test_trsfm)
+        else:  # test
+            dataset = LT_Dataset(data_dir, data_dir + '/malignant.txt', test_trsfm)
             n_val = int(len(dataset) * 0.2)
             n_train = len(dataset) - n_val
-            train_dataset, val_dataset = random_split(dataset, lengths=[n_train, n_val], generator=torch.Generator().manual_seed(0))
+            train_dataset, val_dataset = random_split(dataset, lengths=[n_train, n_val],
+                                                      generator=torch.Generator().manual_seed(0))
             skf = StratifiedKFold(n_splits=5, random_state=None, shuffle=False)
             for train_index, val_index in skf.split(dataset.img_path, dataset.labels):
                 train_dataset.indices = list(train_index)
@@ -129,20 +134,15 @@ class iNaturalistDataLoader(DataLoader):
 
         self.n_samples = len(self.dataset)
         target_list = []
-        for n in range(5):
-            a = []
-            for i in list(train_dataset.indices[n]):
-                a.append(dataset.targets[i])
-            target_list.append(a)
-        num_classes = len(np.unique(target_list[0]))
+        for i in list(train_dataset.indices):
+            target_list.append(dataset.targets[i])
+        num_classes = len(np.unique(target_list))
         # assert num_classes == 8142
+        assert num_classes == 9
 
-        cls_num_list = []
-        for i in target_list:
-            cls_num = [0] * num_classes
-            for label in i:
-                cls_num[label] += 1
-            cls_num_list.append(cls_num)
+        cls_num_list = [0] * num_classes
+        for label in target_list:
+            cls_num_list[label] += 1
 
         self.cls_num_list = cls_num_list
 
@@ -157,7 +157,7 @@ class iNaturalistDataLoader(DataLoader):
                 print("Test set will not be evaluated with balanced sampler, nothing is done to make it balanced")
         else:
             sampler = None
-        
+
         self.shuffle = shuffle
         self.init_kwargs = {
             'batch_size': batch_size,
